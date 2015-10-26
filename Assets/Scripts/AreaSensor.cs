d7f9e37e18fd6b90e0aa24b5fac7b941a1cac5ec
chrischavira <chris@virsix.com>
2015-10-26 09:45:53 -0700

    Initial commit for Hockey demo project.

---
+++ Assets/Scripts/AreaSensor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AreaSensor : MonoBehaviour 
+{
+	public GameObject prefab;
+	public Text shotsTakenText;
+	int shotsTaken = 0;
+	int numObjs = 0;
+	
+	//GameManager gm;
+	
+	void Start()
+	{
+		//gm = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();
+		Instantiate (prefab, transform.position + new Vector3 (0, 1, 0), Quaternion.identity);
+	}
+
+	void OnTriggerExit( Collider coll )
+	{
+		if (coll.tag == "Puck") 
+		{
+			//gm.TookShot();
+			shotsTaken++;
+			shotsTakenText.text = "Shots Taken: " + shotsTaken;
+			numObjs --;
+			if( numObjs <= 0)
+				Instantiate (prefab, transform.position + new Vector3 (0, 1, 0), Quaternion.identity);
+		}
+	}
+
+	void OnTriggerEnter( Collider coll )
+	{
+		if (coll.tag == "Puck")
+			numObjs ++;
+	}
+}
---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour 
+{
+	//GameManager gm;
+	int score = 0;
+	public Text scoreText;
+
+	void Start()
+	{
+		//gm = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();
+	}
+
+	void OnTriggerEnter( Collider coll )
+	{
+		if (coll.tag == "Puck") 
+		{
+			score += 1;
+			scoreText.text = "Score: " + score;
+		}
+			//gm.AddScore (1);
+	}
+}
